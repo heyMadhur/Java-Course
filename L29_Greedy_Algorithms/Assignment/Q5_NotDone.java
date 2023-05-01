a723f375f2789b5bdefa6c6e660a8b75ab0e2484
heyMadhur <madhurgupta12112002@gmail.com>
2023-05-01 19:00:34 +0530

    Ques not Done

---
+++ L29_Greedy_Algorithms/Assignment/Q5_NotDone.java
@@ -0,0 +1,13 @@
+package L29_Greedy_Algorithms.Assignment;
+
+public class Q5_NotDone {
+
+
+    public static void main(String[] args) {
+        // Q5 Split the given array into K sub-arrays
+        // We have an Array[] of N elements and a number K. (1<=K<=N). Split the given array into K subarrays (they must cover all the elements). The maximum subarray sum achievable out of K subarrays formed must be the minimum possible. Find that possible subarray sum.
+        
+        
+    }
+    
+}
