18de1831523ed8189ceb4acb6f91979402261e9d
heyMadhur <madhurgupta12112002@gmail.com>
2023-06-04 09:02:30 +0530

    BST- Search

--- L31_Binary_Search_Tree/BST_Basics.java
+++ L31_Binary_Search_Tree/BST_Basics.java
@@ -30,22 +30,22 @@ public class BST_Basics {
         }
 
         // public void addData(Node root, int value) {
-        //     if (value < root.data) {
-        //         if (root.left != null)
-        //             addData(root.left, value);
-        //         else {
-        //             root.left = new Node(value);
-        //             return;
-        //         }
+        // if (value < root.data) {
+        // if (root.left != null)
+        // addData(root.left, value);
+        // else {
+        // root.left = new Node(value);
+        // return;
+        // }
 
-        //     } else {
-        //         if (root.right != null)
-        //             addData(root.right, value);
-        //         else {
-        //             root.right = new Node(value);
-        //             return;
-        //         }
-        //     }
+        // } else {
+        // if (root.right != null)
+        // addData(root.right, value);
+        // else {
+        // root.right = new Node(value);
+        // return;
+        // }
+        // }
         // }
 
         public Node buildTree(int[] nodes) {
@@ -65,6 +65,21 @@ public class BST_Basics {
             printInOrder(root.right);
         }
 
+        public Node search(Node root, int key) {            // O(H)
+            if (root == null) {
+                return null;
+            }
+            if (key == root.data) {
+                System.out.println("Found");
+                return root;
+            }
+            if (key < root.data) {
+                return search(root.left, key);
+            } else {
+                return search(root.right, key);
+            }
+        }
+
     }
 
     public static void main(String[] args) {
@@ -72,11 +87,14 @@ public class BST_Basics {
         int[] nodes = { 5, 1, 3, 4, 2, 7 };
         BinarySearchTree bst = new BinarySearchTree();
         Node root = bst.buildTree(nodes);
-        System.out.println(root.data);
-        // System.out.println(root.left.data);
-        // System.out.println(root.right.data);
+        // bst.printInOrder(root);
 
-        bst.printInOrder(root);
+        Node search = bst.search(root, 4);
+        if (search == null) {
+            System.out.println("Not Found!!");
+        } else {
+            System.out.println(search.data + " found at " + search);
+        }
 
     }
 }
